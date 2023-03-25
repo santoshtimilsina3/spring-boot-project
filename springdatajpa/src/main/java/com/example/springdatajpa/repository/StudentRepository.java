158ded3b116de8b7412d0677f1c188fe9dd1abcd
santosh <santos.timil@gmail.com>
2023-03-26 02:05:57 +0545

    for-database-update-operation

--- springdatajpa/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ springdatajpa/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -2,12 +2,15 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
-public interface StudentRepository extends JpaRepository<Student,Long > {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
     /*JPQL QUERIES
     They are queried on the basis of class Name not the database table name
@@ -29,7 +32,14 @@ public interface StudentRepository extends JpaRepository<Student,Long > {
 
     //Native Named parameters
     @Query(value = "select * from tbl_student s where s.emailId =:emailId",
-    nativeQuery = true)
+            nativeQuery = true)
     Student getStudentByEmailParam(@Param("emailId") String emailId);
+
+    //For updating the data on the database we need to configure this
+    @Modifying
+    @Transactional
+    @Query(value = "update tbl_student set first_name =?1 where email_address= ?2",
+            nativeQuery = true)
+    int updateStudentFirstNameByEmailId(String firstName, String emailAddress);
 
 }
