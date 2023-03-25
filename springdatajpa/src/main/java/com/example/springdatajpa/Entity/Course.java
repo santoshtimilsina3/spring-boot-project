88d5520d04b4e85a9943a14528b2b3c3259a24aa
santosh <santos.timil@gmail.com>
2023-03-26 03:15:13 +0545

    one to one relationship betn course and course material

---
+++ springdatajpa/src/main/java/com/example/springdatajpa/Entity/Course.java
@@ -0,0 +1,25 @@
+package com.example.springdatajpa.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    @Id
+    @SequenceGenerator(name =
+            "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "course_sequence")
+    private Long courseId;
+    private String title;
+    private Integer credit;
+
+}
