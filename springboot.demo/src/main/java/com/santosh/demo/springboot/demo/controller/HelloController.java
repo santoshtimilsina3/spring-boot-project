ab1dd55e7b431c3af378f0580f1548cafdb81390
santosh <santos.timil@gmail.com>
2023-03-25 19:51:43 +0545

    adding-config-in-properties-file

---
+++ springboot.demo/src/main/java/com/santosh/demo/springboot/demo/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.santosh.demo.springboot.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        logger.info(message);
+        return message;
+    }
+
+}
